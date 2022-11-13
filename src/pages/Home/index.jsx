37b843b442bfcd746d803bb51744238796c4b773
gabriel.santos <gabrieldossantossilva2108@gmail.com>
2022-11-13 18:09:19 -0300

    iniciando estruturacao components e pages

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+import {Menu} from '../../components/Menu';
+
+export const Home = () => {
+
+    return (
+        <>
+            <Menu></Menu>
+            <h1>Home</h1>
+        </>
+    )
+}
