0fe81ef3c6e950de2481253dc73e36e87ba561ad
luchaox <lucasmiguel7@hotmail.com>
2022-11-16 23:27:24 -0300

    alteração para classname

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,15 +4,15 @@ export const Footer = () => {
 
     return (
         <>
-            <footer class="py-3 my-4">
-                <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Home</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Features</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Pricing</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-light">FAQs</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-light">About</a></li>
+            <footer className="py-3 my-4">
+                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-light">Home</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-light">Features</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-light">Pricing</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-light">FAQs</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-light">About</a></li>
                 </ul>
-                <p class="text-center text-light">© 2022 Company, Inc</p>
+                <p className="text-center text-light">© 2022 Company, Inc</p>
             </footer>
 
 
