97ad81a8a1353a8de56169c985011461620c072f
luchaox <lucasmiguel7@hotmail.com>
2022-11-20 23:58:58 -0300

    alterando o header

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -4,18 +4,24 @@ export const Menu = () => {
 
     return (
         <>
-            <nav className="navbar navbar-light shadow">
-                <div className="container-fluid">
-                    <span className="navbar-brand mb-0 h1" />FindTeacher
-
-                <button className="navbar-toggler" type="button">
-                    <span className="navbar-toggler-icon" />
-                </button>
+            <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+            <div class="container px-5">
+                <a class="navbar-brand" href="#page-top"><img src="../../imgs/logo.png" width="200px" /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" 
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                              
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-white" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#!">Cadastro</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#!">Sobre</a></li>
+                    </ul>
                 </div>
-            </nav>
+            </div>
+        </nav>
 
 
-           
+
 
         </>
     )
