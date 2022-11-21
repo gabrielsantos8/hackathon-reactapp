a91371ac23ab4712e9a34ebd8c2f19f022102fd2
luchaox <lucasmiguel7@hotmail.com>
2022-11-21 00:59:55 -0300

    alterando para classname

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -4,17 +4,17 @@ export const Menu = () => {
 
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-            <div class="container px-5">
-                <a class="navbar-brand" href="#page-top"><img src="../../imgs/logo.png" width="200px" /></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" 
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+            <div className="container px-5">
+                <a className="navbar-brand" href="#page-top"><img src="../../imgs/logo.png" width="200px" /></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" 
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                               
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-white" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link text-white" href="#!">Cadastro</a></li>
-                        <li class="nav-item"><a class="nav-link text-white" href="#!">Sobre</a></li>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item"><a className="nav-link text-white" href="#!">Home</a></li>
+                        <li className="nav-item"><a className="nav-link text-white" href="#!">Cadastro</a></li>
+                        <li className="nav-item"><a className="nav-link text-white" href="#!">Sobre</a></li>
                     </ul>
                 </div>
             </div>
