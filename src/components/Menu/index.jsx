8b9eb756e9fbcbb25e4472a4b900542183440b0e
gabriel.santos <gabrieldossantossilva2108@gmail.com>
2022-11-25 23:02:39 -0300

    collapsble

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -7,25 +7,17 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 export const Menu = () => {
 
     return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-            <div className="container px-5">
-                <a className="navbar-brand" href="#page-top"><img src="../../imgs/logo.png" width="200px" /></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" 
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                              
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item"><a className="nav-link text-white" href="/">Home</a></li>
-                        <li className="nav-item"><a className="nav-link text-white" href="/cadastro">Cadastro</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-
-
-
-        </>
+        <Navbar className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home"><img src="../../imgs/logo.png" width="200px" /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="navbar-nav ms-auto">
+                        <Nav.Link className='navitem' href="/">Home</Nav.Link>
+                        <Nav.Link className='navitem' href="/cadastro">Cadastro</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
